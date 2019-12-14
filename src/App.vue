becf03112c731546435fe0923fceaa3ed3a84b5e
Endo_Hizumi <endohizumi0114@gmail.com>
2019-12-15 01:04:06 +0900

    update: I could increase or decrease sushi

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,21 @@
 <template>
   <div id="app">
-    <Sushi msg="Welcome to sushi-go-round App"/>
+    <div id="header">
+      <h1>Welcome to sushi-go-round App</h1>
+      <p>{{message}}</p>
+      <span id="nowState">state:</span>
+      <button id="rotateControll" v-on:click="toggle">{{state}}</button>
+      <p>
+        <span>controller:</span>
+        <button id="add" v-on:click="addSushi">Add</button>
+        <button id="reduce" v-on:click="reduceSushi">Reduce</button>
+      </p>
+    </div>
+    <ul>
+      <li v-for="number in numbers" v-bind:key="number">
+        <Sushi v-bind:state="state" />
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -10,11 +25,34 @@ export default {
   name: "app",
   data() {
     return {
-      state: "spin"
+      state: "spin",
+      message:
+        "Japanese Common People Supporter is kaiten-zushi. I expressed gratitude for that kaiten-zushi.",
+      numbers: [0]
     };
   },
   components: {
     Sushi
+  },
+  methods: {
+    toggle() {
+      if (this.state == "spin") {
+        this.state = "stop";
+        this.message = "oh? were you full of stomach?";
+      } else {
+        this.state = "spin";
+        this.message =
+          "Japanese Common People Supporter is kaiten-zushi. <br> I expressed gratitude for that kaiten-zushi.";
+      }
+    },
+    addSushi() {
+      this.numbers.push(0);
+    },
+    reduceSushi() {
+      if (this.numbers.length > 1) {
+        this.numbers.pop();
+      }
+    }
   }
 };
 </script>
@@ -28,5 +66,4 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-
 </style>
--- src/components/sushi.vue
+++ src/components/sushi.vue
@@ -1,46 +1,21 @@
 <template>
-  <div class="sushi">
-    <div id="sushiArea">
-      <div v-bind:class="state" id="sushi">🍣</div>
-    </div>
-    <h1>Welcome to sushi-go-round App</h1>
-    <p>{{message}}</p>
-    <span id="nowState">state:</span>
-    <button id="rotateControll" v-on:click="toggle">{{state}}</button>
-  </div>
+  <div v-bind:class="state" id="sushi">🍣</div>
 </template>
 
 <script>
 export default {
   name: "Sushi",
-  data() {
-    return {
-      state: "spin",
-      message:
-        "Japanese Common People Supporter is kaiten-zushi. I expressed gratitude for that kaiten-zushi."
-    };
-  },
-  methods: {
-    toggle() {
-      if (this.state == "spin") {
-        this.state = "stop";
-        this.message = "oh? were you full of stomach?";
-      } else {
-        this.state = "spin";
-        this.message =
-          "Japanese Common People Supporter is kaiten-zushi. I expressed gratitude for that kaiten-zushi.";
-      }
-    }
+  props: {
+    state: String
   }
 };
 </script>
 
 <style>
-[id*="sushi"] {
+[id="sushi"] {
   font-family: "Segoe UI Emoji", "Segoe UI Symbol", "Apple Color Emoji",
     "Noto Color Emoji", "Noto Emoji", sans-serif;
-  font-size: 500%;
-  width: 60%;
+  font-size: 1500%;
   margin-left: auto;
   margin-right: auto;
   display: inline-block;
